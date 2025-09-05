b07026b0e92671eb8f165395b2b11dc4ec1762f0
sameerkindia <sameerkhanaa5@gmail.com>
2025-09-05 21:24:20 +0530

    card and guess log item components

---
+++ components/ui/Card.jsx
@@ -0,0 +1,25 @@
+import { StyleSheet, View } from "react-native";
+import Colors from "../../constants/color";
+
+const Card = ({ children }) => {
+  return <View style={styles.card}>{children}</View>;
+};
+
+export default Card;
+
+const styles = StyleSheet.create({
+  card: {
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 16,
+    marginTop: 100,
+    marginHorizontal: 24,
+    backgroundColor: Colors.primary800,
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 0 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+});
